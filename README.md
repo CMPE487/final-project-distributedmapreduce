089df8d2e593721015f1a834610b1726fcba4559
memhak <mehmet.kurtoglu2@boun.edu.tr>
2018-12-20 19:53:55 +0300

    add example script for map reduce syntax

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Else if you want to use the networks resources run:
 ### Disclaimer  
 - Virtual Enironment dependencies are under requirements.txt
 - Need to have Python 3.7 to use the latest modules of asyncio and to run this project
+- Need to have a Linux environment for using the timeout command in Server.py 
+- Good news for Windows users! It also works on Ubuntu Subsystem on Windows :)
+- You can find an example script(example_script.py) for using distributed map reduce with its required syntax
---
+++ example_script.py
@@ -0,0 +1,35 @@
+import time
+import sys
+import hashlib, uuid
+import random
+import requests
+import re
+#### This block is mandatory for our system
+num_ops = 45
+
+offset = int(sys.argv[1])
+limit = int(sys.argv[2])
+####
+
+#courses = ['CMPE140','CMPE140','CMPE150','CMPE150','CMPE150','CMPE150','CMPE150','CMPE150','CMPE210','CMPE220','CMPE250','CMPE300','CMPE322','CMPE343','CMPE344','CMPE350','CMPE422','CMPE425','CMPE434','CMPE436','CMPE443','CMPE446','CMPE451','CMPE451','CMPE475','CMPE480','CMPE483','CMPE487','CMPE489','CMPE491','CMPE492','CMPE49F','CMPE511','CMPE521','CMPE530','CMPE537','CMPE547','CMPE561','CMPE579','CMPE581','CMPE58C','CMPE58D','CMPE58H','CMPE58S','CMPE595']
+courses = ['CMPE140','CMPE150','CMPE210','CMPE220','CMPE250','CMPE300','CMPE322','CMPE343','CMPE344','CMPE350']
+
+
+url = 'http://registration.boun.edu.tr/scripts/quotasearch.asp'
+for i in range(offset,offset+limit,1):
+
+	body = {
+		'abbr': courses[i][:4],
+		'code': courses[i][-3:],
+		'section': "01"
+		}
+
+	r = requests.post(url=url, data = body)
+	result = re.search('Max\. Classroom Capacity:</b> (\d+)<br>',r.text)
+	##### Printing the result is also mandatory
+	print("Capacity for course: " + courses[i] +" is: " + result.group(1))
+	##### As we capture the output from stdout
+
+
+
+
